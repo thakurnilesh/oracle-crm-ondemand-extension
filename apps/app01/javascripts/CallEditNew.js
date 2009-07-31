8b1c2f3137c8d74c776e05c18cfe44a43f613d7d
thakurnilesh <thakurnilesh@gmail.com>
2009-07-31 07:32:36 -0400

    committed25

--- apps/app01/javascripts/CallEditNew.js
+++ apps/app01/javascripts/CallEditNew.js
@@ -248,9 +248,9 @@ function createNewCallActivity(callback){
 	//alert('subjectValue : ' + subjectValue);
 	if(subjectValue == null || subjectValue == '' || contactPerId == null || contactPerId == ''){
 		alert("Subject is the required field!!");
-		var e= $(".fv");
+		var e= $(".fv")[0].append("<span class="requiredText">required</span>");
 		alert('e[0] : ' + e[0]);
-		e[0].append('<span class="requiredText">required</span>');
+		//e[0].append('<span class="requiredText">required</span>');
 		return;
 	}
 	else{
