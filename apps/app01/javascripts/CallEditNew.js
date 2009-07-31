40c441c6ef0d784f0c00ab2c6bd075a86262b013
thakurnilesh <thakurnilesh@gmail.com>
2009-07-31 07:34:17 -0400

    committed26

--- apps/app01/javascripts/CallEditNew.js
+++ apps/app01/javascripts/CallEditNew.js
@@ -248,7 +248,7 @@ function createNewCallActivity(callback){
 	//alert('subjectValue : ' + subjectValue);
 	if(subjectValue == null || subjectValue == '' || contactPerId == null || contactPerId == ''){
 		alert("Subject is the required field!!");
-		var e= $(".fv")[0].append("<span class="requiredText">required</span>");
+		var e= $(".fv")[0].append("<span class='requiredText'>required</span>");
 		alert('e[0] : ' + e[0]);
 		//e[0].append('<span class="requiredText">required</span>');
 		return;
