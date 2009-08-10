f394db775a886e96bf72e8a89fe86ef09db69aa8
thakurnilesh <thakurnilesh@gmail.com>
2009-08-10 04:18:22 -0400

    testN2

--- apps/app01/javascripts/CallEditNew.js
+++ apps/app01/javascripts/CallEditNew.js
@@ -165,7 +165,7 @@ function addNewRowProdDet(){
 	row += "<tr>";
 	row += "<td><span style='color:red' class='requiredText'>Product* </span></td>";
 	row += "<td><select STYLE='width: 130px' id='prodNamePrDet'><option value='none'></option><option value='Arcoxia 120mg'>Arcoxia 120mg</option><option value='Crocin'>Crocin</option><option value='Omez'>Omez</option><option value='Singulair 10x100mg'>Singulair 10x100mg</option><option value='Singulair 20x40mg'>Singulair 20x40mg</option></select></td>";
-	row += "<td><div id=div1 style="visibility:hidden"> Required*</div></td>";
+	row += "<td><div id=div1 style='visibility:hidden'> Required*</div></td>";
 	row += "<td>Priority: </td>";
 	row += "<td><input name=CallProdDetailNew.Priority size='5' tabindex='4' type='text' value='' class=inputControl id='CallProdDetailNew.Priority' /></td>";
 	row += "<td><span style='color:red' class='requiredText'>Indication*:</span></td>";
