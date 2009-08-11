ef2b1481211564de94e727f980a1ab7b5c9dec15
thakurnilesh <thakurnilesh@gmail.com>
2009-08-11 10:46:54 -0400

    show me th lot

--- apps/app01/javascripts/CallEditNew.js
+++ apps/app01/javascripts/CallEditNew.js
@@ -848,6 +848,7 @@ function callWebServToCreateSampDrop(productId, activityId, callback)
 {
 	var prodCategory = document.getElementById('CallSampDropNew.Primary Product Line Name').value;
 	var lotNumber = document.getElementById('CallSampDropNew.LOT Name').value;
+	alert('show me lot: '+lotNumber);
 	var qtyVal = document.getElementById('CallSampDropNew.Quantity').value;
 
 	var fieldsAct = {
