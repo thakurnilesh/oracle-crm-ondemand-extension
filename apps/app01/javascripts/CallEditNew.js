b686c32597ba5c1c327a6e5fb9010e32a7b506ce
thakurnilesh <thakurnilesh@gmail.com>
2009-08-12 04:24:26 -0400

    Comment OwnerID

--- apps/app01/javascripts/CallEditNew.js
+++ apps/app01/javascripts/CallEditNew.js
@@ -333,7 +333,7 @@ function createNewCallActivity(callback){
 		  //  ContactId: "" + contactPerId + "",
 			Objective: "" + objectiveVal + "",
 			Subject: "" + subjectValue + "",
-			OwnerId: "" + ownerId + "",
+			//OwnerId: "" + ownerId + "",
 			Type: "" + typeVal + "",
 			AccountId: "" + accId + "",
 			SmartCall: "" + smartCallId + "",
